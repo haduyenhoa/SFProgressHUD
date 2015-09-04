81b3b7b280bac4a53b180884bd2b1ec441f292de
looseyi <13615033587@126.com>
2015-09-04 16:21:21 +0800

    initial SFProgress

---
+++ SFProgress/SFProgress.h
@@ -0,0 +1,19 @@
+//
+//  SFProgress.h
+//  SFProgress
+//
+//  Created by Edmond on 9/4/15.
+//  Copyright © 2015 XueQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFProgress.
+FOUNDATION_EXPORT double SFProgressVersionNumber;
+
+//! Project version string for SFProgress.
+FOUNDATION_EXPORT const unsigned char SFProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFProgress/PublicHeader.h>
+
+
